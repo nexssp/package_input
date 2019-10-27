a389fc850cde6990fb16a411ec2fd1d2a4227881
Marcin Polak <mapoart@gmail.com>
2019-10-27 15:40:45 +0100

    small fixes.

--- OCR/src/ocr.py
+++ OCR/src/ocr.py
@@ -4,6 +4,7 @@
 import platform
 import json
 import sys
+import os
 
 try:
     from PIL import Image
@@ -32,10 +33,17 @@ def ocr_core(filename):
     )  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
     return text
 
+if not "file" in parsedJson:
+    parsedJson["error"] = "You need to pass image file to parse."
+    exit(1)
 
-resultText = ocr_core(parsedJson["cwd"] + "/" + parsedJson["file"])
 
-parsedJson["Input-OCR-text"] = resultText
+if not ":"+os.path.sep in parsedJson["file"]:
+    resultText = ocr_core(parsedJson["cwd"] + "/" + parsedJson["file"])
+else:
+    resultText = ocr_core(parsedJson["file"])
+    
+parsedJson["text"] = resultText
 NexssStdout = json.JSONEncoder().encode(parsedJson)
 # STDOUT
 sys.stdout.write(NexssStdout)
