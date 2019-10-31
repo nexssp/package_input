aa836685623765ca7d15a1083f0af01a8e42281b
Marcin Polak <mapoart@gmail.com>
2019-10-31 10:58:12 +0100

    Better error handling.

--- OCR/src/ocr.py
+++ OCR/src/ocr.py
@@ -12,7 +12,6 @@ except ImportError:
     import Image
 import pytesseract
 
-
 # STDIN
 NexssStdin = sys.stdin.read()
 
@@ -23,27 +22,26 @@ parsedJson = json.loads(NexssStdin)
 
 # parsedJson["test"] = "test"
 
-
 def ocr_core(filename):
-    """
-    This function will handle the core OCR processing of images.
-    """
     text = pytesseract.image_to_string(
         Image.open(filename)
-    )  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
+    ) 
     return text
 
 if not "file" in parsedJson:
     parsedJson["error"] = "You need to pass image file to parse."
-    exit(1)
-
-
-if not ":"+os.path.sep in parsedJson["file"]:
-    resultText = ocr_core(parsedJson["cwd"] + "/" + parsedJson["file"])
 else:
-    resultText = ocr_core(parsedJson["file"])
+    if not ":"+os.path.sep in parsedJson["file"]:
+        resultText = ocr_core(parsedJson["cwd"] + "/" + parsedJson["file"])
+    else:
+        resultText = ocr_core(parsedJson["file"])
     
-parsedJson["text"] = resultText
+    parsedJson["text"] = resultText
+
 NexssStdout = json.JSONEncoder().encode(parsedJson)
 # STDOUT
 sys.stdout.write(NexssStdout)
+
+if not "file" in parsedJson:
+    exit(1)
+
